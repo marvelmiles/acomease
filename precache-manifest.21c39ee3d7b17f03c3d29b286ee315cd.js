7479c8ce69992db96c7729c48fe436b3226cd45e
Akinrinmola marvellous <marvellousabidemi2@gmail.com>
2020-07-21 00:36:56 +0100

    Updates

---
+++ precache-manifest.21c39ee3d7b17f03c3d29b286ee315cd.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5577ced75e22809b5cf89df9e1f157",
+    "url": "/acomease/index.html"
+  },
+  {
+    "revision": "028c9504a34e9e53881f",
+    "url": "/acomease/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc3319d2a13cbf9a2694",
+    "url": "/acomease/static/css/main.12e38333.chunk.css"
+  },
+  {
+    "revision": "028c9504a34e9e53881f",
+    "url": "/acomease/static/js/2.4ad18cb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/acomease/static/js/2.4ad18cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa799d9bf53c9b49b38",
+    "url": "/acomease/static/js/3.62fdfe36.chunk.js"
+  },
+  {
+    "revision": "dc3319d2a13cbf9a2694",
+    "url": "/acomease/static/js/main.9d80f3d7.chunk.js"
+  },
+  {
+    "revision": "feb421263898b9b0cf8d",
+    "url": "/acomease/static/js/runtime-main.98942191.js"
+  },
+  {
+    "revision": "b08f00470d07670abd06d8e2fe1cf35a",
+    "url": "/acomease/static/media/AM_UIx.b08f0047.png"
+  },
+  {
+    "revision": "974bcc675b18a3443254ae37d05d10cf",
+    "url": "/acomease/static/media/Icon material-add-a-photo.974bcc67.svg"
+  },
+  {
+    "revision": "35caedbc9ab8a5710f4a60392277977a",
+    "url": "/acomease/static/media/Listings@2x.35caedbc.png"
+  },
+  {
+    "revision": "cfa882efa8bb1100c0ca0cb4bf8afed8",
+    "url": "/acomease/static/media/am_Ui.cfa882ef.png"
+  },
+  {
+    "revision": "8f0c9e48a819504ebdc68772746deaf6",
+    "url": "/acomease/static/media/archives.8f0c9e48.png"
+  },
+  {
+    "revision": "d6d1890c2c5b31f5c5c859e403cc7eb3",
+    "url": "/acomease/static/media/be-the-frist-to-know.d6d1890c.jpg"
+  },
+  {
+    "revision": "d55416d1d49c9bd2007d6462b5d368e2",
+    "url": "/acomease/static/media/better-landlord.d55416d1.svg"
+  },
+  {
+    "revision": "99114f18c4feb85bd590deb4d2de6d29",
+    "url": "/acomease/static/media/brand.99114f18.jpg"
+  },
+  {
+    "revision": "35472cfe29ae5232bd3151a7ee8d9d3b",
+    "url": "/acomease/static/media/collect-rent-hero.35472cfe.png"
+  },
+  {
+    "revision": "8a0646261924840e4a446def49524c99",
+    "url": "/acomease/static/media/collect-rent-ui.8a064626.png"
+  },
+  {
+    "revision": "6bdaa6b1a6c9dd6bb30721729147ca18",
+    "url": "/acomease/static/media/dashboard-real-estate-ui-designs-10.6bdaa6b1.jpg"
+  },
+  {
+    "revision": "97fe12289a8fe9ad4ed0cf57773d125a",
+    "url": "/acomease/static/media/dive-in.97fe1228.png"
+  },
+  {
+    "revision": "950371dcc734bdfd25f2ba51eb924c13",
+    "url": "/acomease/static/media/diversity.950371dc.png"
+  },
+  {
+    "revision": "b014341ac3cb1ec480dfc1d4d4c6d43b",
+    "url": "/acomease/static/media/diversity.b014341a.jpg"
+  },
+  {
+    "revision": "706b19265443dc606951234ef99e74ab",
+    "url": "/acomease/static/media/easy-to-use.706b1926.jpg"
+  },
+  {
+    "revision": "10370eadf546e0f258c35f3730d039b4",
+    "url": "/acomease/static/media/free-up-time.10370ead.svg"
+  },
+  {
+    "revision": "9a3949c5f7dd9956a599c9a850bc43e6",
+    "url": "/acomease/static/media/getting-started-payment.9a3949c5.svg"
+  },
+  {
+    "revision": "ed0d6e785402f98354c4c02a22acc706",
+    "url": "/acomease/static/media/hostel.ed0d6e78.htm"
+  },
+  {
+    "revision": "d47836a7afd09b21142815537d3ecc18",
+    "url": "/acomease/static/media/hostel2.d47836a7.jpg"
+  },
+  {
+    "revision": "6dbc0c3d9002d5df67325f74d9570a6b",
+    "url": "/acomease/static/media/housing-agent.6dbc0c3d.png"
+  },
+  {
+    "revision": "b951367422590d876f01e65d933f764b",
+    "url": "/acomease/static/media/interaction-payments.b9513674.svg"
+  },
+  {
+    "revision": "777e4582e914b27e53c6c913562b6dc0",
+    "url": "/acomease/static/media/lodge.777e4582.htm"
+  },
+  {
+    "revision": "a75afdb91de9878c7afa336674a56fe9",
+    "url": "/acomease/static/media/money_transfer_.a75afdb9.jpg"
+  },
+  {
+    "revision": "a93b933a9a381a3f563efd2304b9d9e3",
+    "url": "/acomease/static/media/money_transfer_.a93b933a.png"
+  },
+  {
+    "revision": "ac682da04becfe4cb6b5a4aef00f05b1",
+    "url": "/acomease/static/media/no-wahala.ac682da0.jpg"
+  },
+  {
+    "revision": "4eafca9049f07297fd1c03eb85b91dc3",
+    "url": "/acomease/static/media/peace-of-mind.4eafca90.svg"
+  },
+  {
+    "revision": "8c6c574db247fadea76247987971a90d",
+    "url": "/acomease/static/media/photo.8c6c574d.jpg"
+  },
+  {
+    "revision": "764d813d6b4c6cefdafdc1ed183b95e2",
+    "url": "/acomease/static/media/poperty-owner-db-real-estate-ui-designs-12.764d813d.jpg"
+  },
+  {
+    "revision": "8661dc8ac0cee8e35c6fa0e8981400c2",
+    "url": "/acomease/static/media/regulated.8661dc8a.jpg"
+  },
+  {
+    "revision": "d71cc50c5cde3f552a1b61fca1564708",
+    "url": "/acomease/static/media/savings.d71cc50c.jpg"
+  },
+  {
+    "revision": "d695ef462d5e852909a66f056cf1c406",
+    "url": "/acomease/static/media/show-off-your-place.d695ef46.svg"
+  },
+  {
+    "revision": "102b5b32f9e85717a526fc6cc1be6d0c",
+    "url": "/acomease/static/media/t1.102b5b32.jpg"
+  },
+  {
+    "revision": "aa4d329220a96653dea575312eb4cb17",
+    "url": "/acomease/static/media/t2.aa4d3292.jpg"
+  },
+  {
+    "revision": "579f1704834707d5bdeeaa525fd2eccf",
+    "url": "/acomease/static/media/t3.579f1704.jpg"
+  },
+  {
+    "revision": "a08fc29e3543d2a78503aa46403ed68d",
+    "url": "/acomease/static/media/t4.a08fc29e.jpg"
+  },
+  {
+    "revision": "9b299398b4a0eaca3455e09ea8aaabf4",
+    "url": "/acomease/static/media/t5.9b299398.jpg"
+  },
+  {
+    "revision": "5a45b775c26d0332945b73702d0999c1",
+    "url": "/acomease/static/media/t6.5a45b775.jpg"
+  },
+  {
+    "revision": "b14e982e077fa0ee7694290d97a7fe56",
+    "url": "/acomease/static/media/undraw_Savings_dwkw.b14e982e.png"
+  },
+  {
+    "revision": "2a97d3e735eaf6ae3c2ccf904123718b",
+    "url": "/acomease/static/media/undraw_decide_3iwx.2a97d3e7.png"
+  },
+  {
+    "revision": "b7d31f6455e27535370dfe827fe5865b",
+    "url": "/acomease/static/media/undraw_friends_online_klj6.b7d31f64.png"
+  },
+  {
+    "revision": "b43a4f79c45f04b3bdc0893ec0b410c2",
+    "url": "/acomease/static/media/undraw_mobile_user_7oqo.b43a4f79.png"
+  },
+  {
+    "revision": "a75621adc710e1d1571cccf7c2f12ccf",
+    "url": "/acomease/static/media/undraw_robotics_kep0.a75621ad.png"
+  },
+  {
+    "revision": "cb3a91b012469bfa8bbfa9b76797296f",
+    "url": "/acomease/static/media/undraw_uploading_go67.cb3a91b0.png"
+  },
+  {
+    "revision": "1cb2fb425150fb3fa2a0484c0f64c43c",
+    "url": "/acomease/static/media/user 2.1cb2fb42.svg"
+  },
+  {
+    "revision": "ca5fdc9b925aedcfe19c2d34dcca3fff",
+    "url": "/acomease/static/media/user-blue.ca5fdc9b.png"
+  },
+  {
+    "revision": "2a57e2219aaf4a0a6e6fdb92fd000d46",
+    "url": "/acomease/static/media/user.2a57e221.svg"
+  },
+  {
+    "revision": "9a89a86ac8d883ef0194ac4821c6cec9",
+    "url": "/acomease/static/media/user.9a89a86a.png"
+  },
+  {
+    "revision": "3bfa1df5108841204e53dbe7d2157977",
+    "url": "/acomease/static/media/variety.3bfa1df5.jpg"
+  },
+  {
+    "revision": "ba1376c40d0118d4b68b5200b61ca24a",
+    "url": "/acomease/static/media/vet-agents.ba1376c4.jpg"
+  }
+]);
